dd408cee9d7c53d9c1a2ad6ec044d3ee137a196d
Zakaria El Yamani <zakaria10elyamani@gmail.com>
2025-12-28 00:36:51 +0100

    config

--- admin/js/config.example.js
+++ admin/js/config.example.js
@@ -1,2 +1,5 @@
-export const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_YOUR_KEY_HERE";
+const SUPABASE_URL = "https://wvkpyimmnpjexiavxrf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_pTlhDZxq5CblFjjBRqpcQ_ua6clP7c";
+
+/*export const SUPABASE_URL = "https://YOUR_PROJECT_ID.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_YOUR_KEY_HERE";*/
