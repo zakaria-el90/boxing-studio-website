463ecc45c2f863dc5d830a1e36f75ba3e4873fec
Zakaria El Yamani <zak8022s@hs-coburg.de>
2026-01-11 18:39:03 +0100

    Phase 3

--- public/admin/js/supabaseClient.js
+++ public/admin/js/supabaseClient.js
@@ -1,11 +1,8 @@
 //admin/js/supabaseClient.js
-/*import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 import { SUPABASE_URL, SUPABASE_KEY } from "./config.js"
 
 export const supabase = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
-);*/
-
-export const SUPABASE_URL = "https://wvkpyyimmnpjexiavxrf.supabase.co";
-export const SUPABASE_KEY = "sb_publishable_pTlhDqZxq5CblFjjBRqpcQ_ua6clP7c";
+);st SUPABASE_KEY = "sb_publishable_pTlhDqZxq5CblFjjBRqpcQ_ua6clP7c";
---
+++ public/js/media-config.js
@@ -0,0 +1,2 @@
+window.SUPABASE_URL = "https://wvkpyyimmnpjexiavxrf.supabase.co";
+window.SUPABASE_KEY = "sb_publishable_pTlhDqZxq5CblFjjBRqpcQ_ua6clP7c";
