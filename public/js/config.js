1520c80bfde97fa9bcfabfa00204a9b3584e23f1
Zakaria El Yamani <zak8022s@hs-coburg.de>
2026-01-14 19:13:18 +0100

    commitar

---
+++ public/js/config.js
@@ -0,0 +1,4 @@
+console.log("config.js loaded");
+
+export const SUPABASE_URL = "https://wvkpyyimmnpjexiavxrf.supabase.co";
+export const SUPABASE_KEY = "sb_publishable_pTlhDqZxq5CblFjjBRqpcQ_ua6clP7c";
